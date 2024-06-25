8bc4f495d6d3ae44589e3245a3b723fdeff0f1bd
Shivam07036038 <143093843+Shivam07036038@users.noreply.github.com>
2024-06-25 22:01:52 +0530

    huihuihui

---
+++ company/apple dark.theme.css
@@ -0,0 +1,49 @@
+/**
+ * @name Apple Dark
+ * @author Solta
+ * @version 1.0.0
+ * @description Тема с Apple
+*/
+
+/*
+ * Если вы нашли ошибку или лишний код, напишите мне, контакты выше.
+*/
+
+/* ↓ Шрифт ↓ */
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap");
+/* ↓ Статус пользователя в кружке ↓ */
+@import url("https://discordstyles.github.io/SoftX/RadialGlow.css");
+/* ↓ Вертикальное расположение аватара, значоков мута микрофона/настроек ↓ */
+@import url("https://discordstyles.github.io/SoftX/VerticalUserArea.css"); 
+/* ↓ Верхний бар со значком дискорд/поиск/закрыть/свернуть ↓ */
+@import url("https://discordstyles.github.io/Addons/windows-titlebar.css");
+/* ↓ Основа ↓ */
+@import url("https://mwittrien.github.io/BetterDiscordAddons/Themes/BasicBackground/BasicBackground.theme.css");
+
+:root {
+  --background: url("https://So1ta.github.io/Images/apple.jpg"); /* Задний фон (Пометка: если меняете на свой, лучше используйте ссылку как у меня, иначе изображение не загрузится) */
+  --font:                       "Inter";      /* Шрифт */
+  --transparencyalpha:          0.6;          /* Блюр заднего фона (общего) */
+  --transparencycolor:			    0,0,0;				/* Цвет заднего фона (общего) */
+	--messagetransparency:			  0;						/* Блюр заднего фона за сообщениями */
+	--guildchanneltransparency:		0;						/* Блюр заднего фона доролнительный	*/
+	--chatinputtransparency:		  0;				  	/* Блюр заднего фона input чата */
+	--memberlisttransparency:		  0.3;					/* Блюр заднего фона участников	*/
+	--accentcolor:					      110,110,110;	/* Общий цвет */	
+  --accentcolor2:					      205, 92, 92 !important;   /* Общий цвет 2*/
+	--mentioncolor:					      205, 92, 92 !important;
+	--linkcolor:					        51,102,187 !important;	  /* Цвет ссылок */
+	--settingsicons:				      1;						/* Иконки в настройках 1 - вкл, 0 - выкл	*/
+}
+
+.theme-dark {
+  --text-normal:        #ffff;      /* Цвет текста в сообщениях */
+  --text-muted:         #ffff;      /* Цвет даты сообщения */
+  --header-primary:     #ffff;      /* Цвет доп. текста */
+  --channels-default:   #ffff;      /* Цвет доп. иконок и текста */      
+  --header-secondary:   #ffff;      /* Цвет некоторого текста в настройках */
+  --interactive-normal: #ffff;      /* Цвет иконок */
+  --interactive-hover:  #ffff;      /* Цвет текста выбранного канала ПРИ НАВЕДЕНИИ МЫШИ либо значков мута микрофона (Имеется ввиду чата или голосового канала) */
+  --interactive-active: #ffff;      /* Цвет текста выбранного канала (Имеется ввиду чата или голосового канала) */
+  --background-modifier-accent: #fff2;        /* Цвет полосы между сообщениями, участниками */
+}
